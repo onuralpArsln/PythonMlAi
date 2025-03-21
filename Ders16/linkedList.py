9283cf58c5fc04a25824e7902d1639a29aff1259
hackerFaruk <onuralparslan97@gmail.com>
2025-03-21 22:29:27 +0300

    linked list

---
+++ Ders16/linkedList.py
@@ -0,0 +1,50 @@
+class Node:
+    val:int
+    nextNode:"Node" = None
+
+if __name__ == "__main__":
+    firstNode=Node()
+    firstNode.val=6
+
+    secondNode=Node()
+    secondNode.val=7
+
+    thirdNode=Node()
+    thirdNode.val=8
+
+    firstNode.nextNode=secondNode
+    secondNode.nextNode=thirdNode
+
+    currentNode=firstNode
+
+    while currentNode is not None:
+        print(currentNode.val)
+        currentNode=currentNode.nextNode
+
+    print("second node silmek")
+
+    firstNode.nextNode=thirdNode
+
+    currentNode=firstNode
+
+    while currentNode is not None:
+        print(currentNode.val)
+        currentNode=currentNode.nextNode
+
+    print("listede pop yapmak")
+
+    
+    firstNode.nextNode=secondNode
+    secondNode.nextNode=thirdNode
+    # pop() yaptık sonuncu silindi
+    secondNode.nextNode=None
+
+    
+    currentNode=firstNode
+
+    while currentNode is not None:
+        print(currentNode.val)
+        currentNode=currentNode.nextNode
+
+
+
